1eb538a26f4afeaa06fc8973d51673b536b62d66
Mirosław Błażej <miro662@gmail.com>
2021-12-12 16:48:41 +0100

    Add vertex dataclass

---
+++ gg_project/vertex_params.py
@@ -0,0 +1,24 @@
+""" Contains types used to store vertex parameters
+"""
+
+from dataclasses import dataclass
+from enum import Enum
+
+
+class VertexType(str, Enum):
+    """Describes the type of a vertex"""
+
+    START = "S"
+    START_USED = "s"
+    EXTERIOR = "E"
+    INTERIOR = "I"
+    INTERIOR_USED = "i"
+
+
+@dataclass
+class VertexParams:
+    """Contains internal parameters of a vertex"""
+
+    vertex_type: VertexType
+    position: tuple[float, float]
+    level: int
